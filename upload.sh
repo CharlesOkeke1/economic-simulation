cc7908808d7863d0d0976938a980a5830ebd7e73
Charles Okeke <charles.okeke@warwick.ac.uk>
2026-02-23 12:35:38 +0000

    Updated Upload script to include tags and added a changelog file

---
+++ upload.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Ask for commit message
+read -p "Enter commit message: " message
+read -p "Enter Version tag: " vTag
+read -p "Enter tag message: " tagMessage
+
+# Stop if message is empty
+if [ -z "$message" ]; then
+    echo "Commit message cannot be empty."
+    exit 1
+fi
+
+echo "Adding changes..."
+git add .
+
+echo "Committing..."
+git commit -m "$message"
+
+echo "tagging..."
+git tag -a "$vTag" -m "$tagMessage"
+
+echo "Pushing to origin/main..."
+git push origin main
+
+echo "Upload complete."
